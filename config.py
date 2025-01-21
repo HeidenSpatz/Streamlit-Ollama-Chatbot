c6992f8f8b66c6f6be1b69af2f0e4e1e7f8b13fd
Robert Uwe Litschel <uwelitschel@gmail.com>
2025-01-21 22:22:22 +0100

    config available models

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ https://www.youtube.com/@DevTechBytes
 class Config:
     PAGE_TITLE = "Streamlit Ollama Chatbot"
 
-    OLLAMA_MODELS = ('codellama:7b', 'codellama:13b', 'llama2-uncensored', 
-                    'llama2:7b', 'llama2:13b', 'mistral', 'mixtral')
+    OLLAMA_MODELS = ('llama3.2:latest', 'deepseek-r1:32b', 'phi4:latest')
 
     SYSTEM_PROMPT = f"""You are a helpful chatbot that has access to the following 
                     open-source models {OLLAMA_MODELS}.
